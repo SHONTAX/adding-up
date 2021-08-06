be1cb4a7e487b5e66ce9e4a6b45621945fd61250
SHONTAX <shontax.mail@gmail.com>
2021-08-06 14:08:11 +0900

    ランキングの実装完了

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const readline = require('readline');
 // ストリーム作成
 const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ input: rs, output: {} });
-const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
+// key: 都道府県 value: 集計データのオブジェクト
+const prefectureDataMap = new Map(); 
 
 // rl オブジェクトで line というイベントが発生したら この無名関数を呼んでください
 rl.on('line', lineString => {
-    //console.log(lineString);
     const columns = lineString.split(',');
     const year = parseInt(columns[0]);
     const prefecture = columns[1];
@@ -33,24 +33,20 @@ rl.on('line', lineString => {
     }
 });
 rl.on('close', () => {
+    // 第一要素の key という変数にキーを、第二要素の value という変数に値を代入
     for (const [key, value] of prefectureDataMap){
         value.change = value.popu15 / value.popu10;
     }
-    // 連想関数を普通の配列に変換
+    // 連想関数を普通の配列に変換 ?
     const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
         return pair2[1].change - pair1[1].change;
     });
     //
     const rankingStrings = rankingArray.map(([key, value]) => {
         return (
-            key +
-            ':' +
-            value.popu10 +
-            '=>' +
-            value.popu15 +
-            ' 変化率:' +
-            value.change
+            key + ':' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change
         );
     });
-    console.log(rankingStrings);
+    //console.log(rankingStrings);
+    console.log(rankingArray);
 });
